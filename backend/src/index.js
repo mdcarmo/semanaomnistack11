009f68e72e5f2bdaa6f7242f61806f56e0e4be63
Dias do Carmo <marcelo.carmo@scania.com>
2020-03-24 18:49:27 -0300

    inclusão do cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
-// app.get("/", (request, response) => {
-//   return response.send("Hello Word");
-// });
 
 /**
  * Tipos de parametros
